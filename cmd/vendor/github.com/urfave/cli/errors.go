b99c022872e58db9ff792188ae2b64cdf34d6434
Justin Rudio <justinjrudio@gmail.com>
2016-08-04 02:07:11 -0700

    Updated to new cli package

--- cmd/vendor/github.com/urfave/cli/errors.go
+++ cmd/vendor/github.com/urfave/cli/errors.go
@@ -2,18 +2,29 @@ package cli
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
+// OsExiter is the function used when the app exits. If not set defaults to os.Exit.
+var OsExiter = os.Exit
+
+// ErrWriter is used to write errors to the user. This can be anything
+// implementing the io.Writer interface and defaults to os.Stderr.
+var ErrWriter io.Writer = os.Stderr
+
+// MultiError is an error that wraps multiple errors.
 type MultiError struct {
 	Errors []error
 }
 
+// NewMultiError creates a new MultiError. Pass in one or more errors.
 func NewMultiError(err ...error) MultiError {
 	return MultiError{Errors: err}
 }
 
+// Error implents the error interface.
 func (m MultiError) Error() string {
 	errs := make([]string, len(m.Errors))
 	for i, err := range m.Errors {
@@ -26,6 +37,7 @@ func (m MultiError) Error() string {
 // ExitCoder is the interface checked by `App` and `Command` for a custom exit
 // code
 type ExitCoder interface {
+	error
 	ExitCode() int
 }
 
@@ -56,20 +68,31 @@ func (ee *ExitError) ExitCode() int {
 }
 
 // HandleExitCoder checks if the error fulfills the ExitCoder interface, and if
-// so prints the error to stderr (if it is non-empty) and calls os.Exit with the
+// so prints the error to stderr (if it is non-empty) and calls OsExiter with the
 // given exit code.  If the given error is a MultiError, then this func is
 // called on all members of the Errors slice.
 func HandleExitCoder(err error) {
+	if err == nil {
+		return
+	}
+
 	if exitErr, ok := err.(ExitCoder); ok {
 		if err.Error() != "" {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(ErrWriter, err)
 		}
-		os.Exit(exitErr.ExitCode())
+		OsExiter(exitErr.ExitCode())
+		return
 	}
 
 	if multiErr, ok := err.(MultiError); ok {
 		for _, merr := range multiErr.Errors {
 			HandleExitCoder(merr)
 		}
+		return
 	}
+
+	if err.Error() != "" {
+		fmt.Fprintln(ErrWriter, err)
+	}
+	OsExiter(1)
 }
