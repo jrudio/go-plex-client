e5b4386beb175045f98d134b7a459afbcbaa5d57
Justin Rudio <justinjrudio@gmail.com>
2022-04-27 22:24:13 -0700

    fixed the types for metadata json responses (#35)
    * fixed the types for metadata json responses
    
    * added GetPlaylist() to cli
    
    * added GetPlaylist() to cli
    
    * fixed GetPlaylist() - catch non-200 status http errors
    
    * changed types on properties that are int or string
    
    * changed RatingCount per #41
    
    * handle OptimizedForStreaming bool or int

--- models.go
+++ models.go
@@ -3,6 +3,7 @@ package plex
 import (
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -68,7 +69,7 @@ type Metadata struct {
 	ParentRatingKey       string       `json:"parentRatingKey"`
 	ParentThumb           string       `json:"parentThumb"`
 	ParentTitle           string       `json:"parentTitle"`
-	RatingCount           string       `json:"ratingCount"`
+	RatingCount           int          `json:"ratingCount"`
 	Rating                float64      `json:"rating"`
 	RatingKey             string       `json:"ratingKey"`
 	SessionKey            string       `json:"sessionKey"`
@@ -91,6 +92,37 @@ type AltGUID struct {
 	ID string `json:"id"`
 }
 
+type boolOrInt struct {
+	bool
+}
+
+func (b *boolOrInt) UnmarshalJSON(data []byte) error {
+	var isInt int
+
+	if err := json.Unmarshal(data, &isInt); err == nil {
+		if isInt == 0 || isInt == 1 {
+
+			if isInt != 0 && isInt != 1 {
+				return fmt.Errorf("invalid boolOrInt: %d", isInt)
+			}
+
+			b.bool = isInt == 1
+
+			return nil
+		}
+	}
+
+	var isBool bool
+
+	if err := json.Unmarshal(data, &isBool); err != nil {
+		return err
+	}
+
+	b.bool = isBool
+
+	return nil
+}
+
 // Media media info
 type Media struct {
 	AspectRatio           json.Number `json:"aspectRatio"`
@@ -103,7 +135,8 @@ type Media struct {
 	Has64bitOffsets       bool        `json:"has64bitOffsets"`
 	Height                int         `json:"height"`
 	ID                    json.Number `json:"id"`
-	OptimizedForStreaming bool        `json:"optimizedForStreaming"` // plex can return int or boolean: 0 or 1; true or false
+	OptimizedForStreaming boolOrInt   `json:"optimizedForStreaming"` // plex can return int (GetMetadata(), GetPlaylist()) or boolean (GetSessions()): 0 or 1; true or false
+
 	Selected              bool        `json:"selected"`
 	VideoCodec            string      `json:"videoCodec"`
 	VideoFrameRate        string      `json:"videoFrameRate"`
@@ -821,7 +854,7 @@ type Part struct {
 	HasThumbnail          string      `json:"hasThumbnail"`
 	ID                    json.Number `json:"id"`
 	Key                   string      `json:"key"`
-	OptimizedForStreaming bool        `json:"optimizedForStreaming"`
+	OptimizedForStreaming boolOrInt   `json:"optimizedForStreaming"`
 	Selected              bool        `json:"selected"`
 	Size                  int         `json:"size"`
 	Stream                []Stream    `json:"Stream"`
